cae766186176e2762bf97a7cb2ef224f0f8ea96a
athazayyan <achmadatha@mhs.usk.ac.id>
2025-07-30 02:05:35 +0700

    tambah no rek

--- models/VerifikasiSeller.js
+++ models/VerifikasiSeller.js
@@ -1,32 +1,43 @@
 const mongoose = require("mongoose");
 
-const verifikasiSellerSchema = new mongoose.Schema({
-  user: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-    unique: true
+const verifikasiSellerSchema = new mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+      unique: true,
+    },
+    npwp: {
+      type: String,
+      required: true,
+    },
+    ktp: {
+      type: String,
+      required: true,
+    },
+    dokumenBisnis: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      enum: ["pending", "approved", "rejected"],
+      default: "pending",
+    },
+    no_rekening: {
+      type: String,
+      required: true,
+    },
+    nama_rekening: {
+      type: String,
+      required: true,
+    },
+    catatan: {
+      type: String,
+    },
   },
-  npwp: {
-    type: String,
-    required: true
-  },
-  ktp: {
-    type: String,
-    required: true
-  },
-  dokumenBisnis: {
-    type: String,
-    required: true
-  },
-  status: {
-    type: String,
-    enum: ["pending", "approved", "rejected"],
-    default: "pending"
-  },
-  catatan: {
-    type: String
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("VerifikasiSeller", verifikasiSellerSchema);
