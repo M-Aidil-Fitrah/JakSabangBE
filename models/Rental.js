fc2cbed6df3f0d6e9d632a01d062b2eca0df45a8
athazayyan <achmadatha@mhs.usk.ac.id>
2025-08-01 19:25:08 +0700

    ubah user model

--- models/Rental.js
+++ models/Rental.js
@@ -1,38 +1,41 @@
 const mongoose = require("mongoose");
 
-const rentalSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
+const rentalSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: ["motor", "mobil", "driver"],
+      required: true,
+    },
+    harga: {
+      type: Number,
+      required: true,
+    },
+    deskripsi: {
+      type: String,
+    },
+    gambar: {
+      type: String,
+    },
+    penyedia: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    namaPenyedia: {
+      type: String,
+      required: true,
+    },
+    no_telepon: {
+      type: String,
+      required: true,
+    },
   },
-  type: {
-    type: String,
-    enum: ["motor", "mobil", "driver"],
-    required: true,
-  },
-  harga: {
-    type: Number,
-    required: true,
-  },
-  deskripsi: {
-    type: String,
-  },
-  gambar: {
-    type: String, // URL dari Cloudinary
-  },
-  penyedia: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-    required: true,
-  },
-  namaPenyedia: {
-    type: String,
-    required: true,
-  },
-  no_telepon: {
-    type: String,
-    required: true,
-  }
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Rental", rentalSchema);
--- models/User.js
+++ models/User.js
@@ -1,33 +1,33 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: true,
-  },
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  no_hp: {
-  type: String,
-  required: true,
-}
-,
-    alamat: {
-    type: String,
-    required: true,
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
     },
-  role: {
-    type: String,
-    enum: ["buyer", "seller", "admin"],
-    required: true,
+    email: {
+      type: String,
+      unique: true,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    no_hp: {
+      type: String,
+    },
+    alamat: {
+      type: String,
+    },
+    role: {
+      type: String,
+      enum: ["buyer", "seller", "admin"],
+      required: true,
+    },
   },
-}, { timestamps: true });
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("User", userSchema);
