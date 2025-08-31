fc6ffc9200de1088c4ba2df4c4b2784e249495df
naufalhanif25 <naufal.hanif2525@gmail.com>
2025-08-31 08:13:00 +0700

    docs: update Swagger UI docs

--- routes/tokenRoutes.js
+++ routes/tokenRoutes.js
@@ -3,6 +3,44 @@ const { checkToken } = require("../controllers/tokenController");
 
 const router = express.Router();
 
+/**
+ * @swagger
+ * /api/token:
+ *   post:
+ *     summary: Cek masa aktif token
+ *     tags: [Token]
+ *     security:
+ *       - bearerAuth: []
+ *     responses:
+ *       200:
+ *         description: Status token dikembalikan
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 active:
+ *                   type: boolean
+ *                   example: true
+ *                 token:
+ *                   type: string
+ *                   example: "eyJhbGciOiJIUzI1NiIsInR5cCI..."
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     id:
+ *                       type: string
+ *                       example: "62834e83253eb4b8a4c2fc05"
+ *                     role:
+ *                       type: string
+ *                       example: "buyer"
+ *                     iat:
+ *                       type: number
+ *                       example: 1755545789
+ *                     exp:
+ *                       type: number
+ *                       example: 1765632389
+ */
 router.get("/", checkToken);
 
 module.exports = router;
